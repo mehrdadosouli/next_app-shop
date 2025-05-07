198d5358f3312514084ce6c6b8ba1849192559c1
mehrdad <mehrdadosoulisaran@gmail.com>
2025-05-07 13:44:30 +0330

    set footer base

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import Header from "@/layouts/Header";
 import "./globals.css";
+import Header from "@/layouts/Header";
+import Footer from "@/layouts/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-      <Header>
+      <Header />
         {children}
-      </Header>
+        <Footer />
       </body>
     </html>
   );
