8ec6ca4397937970133900b4a7fbfd75e679aae4
mehrdad <mehrdadosoulisaran@gmail.com>
2025-05-07 13:36:31 +0330

    set header layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
+import Header from "@/layouts/Header";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-
+      <Header>
         {children}
-
+      </Header>
       </body>
     </html>
   );
