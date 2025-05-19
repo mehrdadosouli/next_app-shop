a8fca399a55318cf39b8de8f27971716b577d3ab
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2025-05-19 18:57:37 +0330

    fixeed bog api product page in route.js

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -6,15 +6,15 @@ export async function GET(req) {
   const url=new URL(req.url)  
   const query = url.searchParams;
   const searchTerm= query.get("category");
-   
+  
   try {
     await dbConnect();
     let products 
     if (searchTerm) {           
-       products = await Product.find({category:searchTerm});
-    }else{
-       products = await Product.find({});
+      products = await Product.find({category:searchTerm});
+      console.log("hi");
     }
+      products = await Product.find({});
     return NextResponse.json(products);
   } catch (error) {
     return NextResponse.json({error:"حطا در گرفتن محصولات"});
