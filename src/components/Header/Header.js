f27b39b6d35057025b6126673bbd40a2f72f06ff
mehrdad <mehrdadosoulisaran@gmail.com>
2025-05-26 08:29:36 +0330

    set handler onclick for button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,17 +8,24 @@ import { usePathname } from "next/navigation"
 function Header() {
     let pathname=usePathname() 
     const [show, setShow] = useState(false)
-    const clickHandler = (e) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const allowedTags = ["a", "span", "img", "button", "svg", "path"];
+    const clickHandler = () => {
         setShow(!show)
     }
     useEffect(() => {
-        window.addEventListener('click',(e)=>{            
-            if(e.target.tagName !== "A" && e.target.tagName !== "SPAN" && e.target.tagName != "IMG"){
-                setShow(false)
+        window.addEventListener('click',(e)=>{ 
+            e.stopPropagation();
+            if(!allowedTags.includes(e.target.tagName.toLowerCase())){
+                setShow(false);
+                setIsOpen(false);
             }
         })
     }, [])
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
    
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
@@ -52,23 +59,23 @@ function Header() {
                             </li>
                         </ul>
                     </div>
-                    <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-user" onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <svg className="w-5 h-5" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
                 </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+                <div className={`items-center justify-between ${!isOpen && "hidden"} w-full md:flex md:w-auto md:order-1`} id="navbar-user">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link href="/" className={`block py-2 px-3 ${pathname === '/' ? 'text-blue-700' : 'text-gray-900'} bg-blue-700 rounded-sm md:bg-transparent md:p-0 md:dark:text-blue-500`}>صفحه اصلی</Link>
+                            <Link href="/" className={`block py-2 px-3 ${pathname === '/' ? 'text-blue-400 bg-blue-700' : 'text-gray-900'} rounded-sm md:bg-transparent md:p-0 md:dark:text-blue-500`}>صفحه اصلی</Link>
                         </li>
                         <li>
-                            <Link href="/shopping" className={`block py-2 px-3 ${pathname === '/shopping' ? 'text-blue-700' : 'text-gray-900'} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>فروشگاه</Link>
+                            <Link href="/shopping" className={`block py-2 px-3 ${pathname === '/shopping' ? 'text-blue-400  bg-blue-700' : 'text-gray-900'} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>فروشگاه</Link>
                         </li>
                         <li>
-                            <Link href="/basket" className={`block py-2 px-3 ${pathname === '/basket' ? 'text-blue-700' : 'text-gray-900'} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>سبد خرید</Link>
+                            <Link href="/basket" className={`block py-2 px-3 ${pathname === '/basket' ? 'text-blue-400 bg-blue-700' : 'text-gray-900'} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>سبد خرید</Link>
                         </li>
                     </ul>
                 </div>
