c7802f5b14907815c05aeba4fef46691395faf6d
mehrdad <mehrdadosoulisaran@gmail.com>
2025-05-29 11:20:35 +0330

    set default svg for when not picture for product

--- src/components/CardProduct/CardProduct.js
+++ src/components/CardProduct/CardProduct.js
@@ -7,18 +7,32 @@ import Score from '../Score/Score';
 
 function CardProduct({ data }) {
   let { addToCard } = useContext(CartContext);
-  const isValidSrc=typeof data.image == "string" && (data.image.startsWith("http://") || data.image.startsWith("https://") || data.image.startsWith("/"))
-  const [imgSrc, setImgSrc] = useState(isValidSrc? data.image : "/img.jpg");
+  const isValidSrc =
+    typeof data.image == 'string' && (data.image.startsWith('http://') || data.image.startsWith('https://') || data.image.startsWith('/'));
+  const [imgSrc, setImgSrc] = useState(isValidSrc ? data.image : null);
   return (
     <div className="w-full flex flex-col justify-between  bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
       <Link href={`shopping/${data._id}`} className="mx-auto">
-        <Image className="p-8 rounded-t-lg object-cover" src={imgSrc} width={200} height={150} alt={data.title || "product image"} />
+        {imgSrc ? (
+          <Image className="rounded-t-lg object-cover" src={imgSrc} width={100} height={100} alt={data.title} />
+        ) : (
+          <div className="w-24 h-24 text-gray-400 flex items-center justify-center rounded-t-lg bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-16 h-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.5 6h13L17 13M7 13h10"
+              />
+            </svg>
+          </div>
+        )}
       </Link>
       <div className="px-5 pb-5">
         <Link href={`shopping/${data._id}`}>
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{data.title}</h5>
         </Link>
-        <Score rating={data.rating}/>
+        <Score rating={data.rating} />
         <div className="flex items-center justify-between">
           <span className="text-2xl font-bold text-gray-900 dark:text-white">{data.price.toLocaleString('fa-IR') + ' تومان'}</span>
           <button
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,41 @@
-"use client"
-import Link from "next/link"
-import Image from "next/image"
-import { useContext, useEffect, useState, useRef } from "react"
-import { usePathname } from "next/navigation"
-import { CartContext } from "@/app/contexts/CartContext"
-import img1 from "../../../public/img.jpg"
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useContext, useEffect, useState, useRef } from 'react';
+import { usePathname } from 'next/navigation';
+import { CartContext } from '@/app/contexts/CartContext';
+import img1 from '../../../public/img.jpg';
 
 function Header() {
-  const basketRef = useRef(null)
-  const pathname = usePathname()
-  const { cart, addToCard, removeFromCard, minusFromCard, total } = useContext(CartContext)
+  const pathname = usePathname();
+  const { cart, addToCard, removeFromCard, minusFromCard, total } = useContext(CartContext);
 
-  const [isOpenBasket, setIsOpenBasket] = useState(false)
-  const [isOpenMenu, setIsOpenMenu] = useState(false)
-  const [showUserMenu, setShowUserMenu] = useState(false)
+  const [isOpenBasket, setIsOpenBasket] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [showUserMenu, setShowUserMenu] = useState(false);
 
-  const toggleBasket = () => setIsOpenBasket((prev) => !prev)
-  const toggleMenu = () => setIsOpenMenu((prev) => !prev)
-  const toggleUserMenu = () => setShowUserMenu((prev) => !prev)
+  const toggleBasket = () => setIsOpenBasket((prev) => !prev);
+  const toggleMenu = () => setIsOpenMenu((prev) => !prev);
+  const toggleUserMenu = () => setShowUserMenu((prev) => !prev);
 
   // بستن منوی سبد خرید اگر کلیک بیرون از آن شد
   useEffect(() => {
-      
-      function handleClickOutside(event) {  
-        if(event.target.id == "blur"){
-            setIsOpenBasket(false)
-        }        
-      if (
-        !event.target.closest("#user-menu-button") &&
-        !event.target.closest("#user-dropdown")
-      ) {
-        setShowUserMenu(false)
+    function handleClickOutside(event) {
+      if (event.target.id == 'blur') {
+        setIsOpenBasket(false);
+      }
+      if (!event.target.closest('#user-menu-button') && !event.target.closest('#user-dropdown')) {
+        setShowUserMenu(false);
       }
     }
 
     if (isOpenBasket || showUserMenu) {
-      window.addEventListener("click", handleClickOutside)
+      window.addEventListener('click', handleClickOutside);
     }
     return () => {
-      window.removeEventListener("click", handleClickOutside)
-    }
-  }, [isOpenBasket, showUserMenu ])
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpenBasket, showUserMenu]);
 
   return (
     <>
@@ -84,26 +79,25 @@ function Header() {
                       className="w-full flex flex-col justify-between bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 p-3"
                     >
                       <Link href={`shopping/${item.id}`}>
-                        <Image
-                          className="rounded-t-lg object-cover"
-                          src={item.image}
-                          width={200}
-                          height={150}
-                          alt={item.title}
-                        />
+                        {typeof item.image === 'string' &&
+                        (item.image.startsWith('http://') || item.image.startsWith('https://') || item.image.startsWith('/')) ? (
+                          <Image className="rounded-t-lg object-cover" src={item.image} width={100} height={100} alt={item.title} />
+                        ) : (
+                          <div className="w-24 h-24 text-gray-400 flex items-center justify-center rounded-t-lg bg-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-16 h-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.5 6h13L17 13M7 13h10"/>
+                            </svg>
+                          </div>
+                        )}
                       </Link>
                       <div className="px-2 py-1">
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                          {item.title}
-                        </h5>
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
                         <div className="flex items-center justify-between mt-2.5 mb-3">
-                          <span className="text-2xl font-bold text-gray-900 dark:text-white select-none">
-                            {item.price} $
-                          </span>
+                          <span className="text-2xl font-bold text-gray-900 dark:text-white select-none">{item.price} $</span>
                           <div className="flex items-center gap-3">
                             {item.quantity > 1 ? (
                               <>
-                                <button onClick={() => minusFromCard(item)} className="p-1 bg-gray-200 rounded" >
+                                <button onClick={() => minusFromCard(item)} className="p-1 bg-gray-200 rounded">
                                   -
                                 </button>
                                 <span className="select-none">{item.quantity}</span>
@@ -116,7 +110,7 @@ function Header() {
                                 <button onClick={() => removeFromCard(item)} className="p-1 bg-red-600 text-white rounded">
                                   حذف
                                 </button>
-                                <button onClick={() => addToCard(item)} className="p-1 bg-blue-700 text-white rounded" >
+                                <button onClick={() => addToCard(item)} className="p-1 bg-blue-700 text-white rounded">
                                   افزودن
                                 </button>
                               </>
@@ -127,28 +121,14 @@ function Header() {
                     </div>
                   ))
                 ) : (
-                  <div className="text-center p-4 text-gray-700 dark:text-gray-200">
-                    سبد خالی است
-                  </div>
+                  <div className="text-center p-4 text-gray-700 dark:text-gray-200">سبد خالی است</div>
                 )}
-                {cart.length > 0 && (
-                  <div className="p-2 select-none font-bold text-right">
-                    جمع خرید : {total()} $
-                  </div>
-                )}
+                {cart.length > 0 && <div className="p-2 select-none font-bold text-right">جمع خرید : {total()} $</div>}
               </div>
             )}
 
-            <Image
-              src="/img2.jpg"
-              width={40}
-              height={40}
-              className="h-8 rounded-full"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Mehrdad
-            </span>
+            <Image src="/img2.jpg" width={40} height={40} className="h-8 rounded-full" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mehrdad</span>
           </div>
 
           {/* منوی کاربری */}
@@ -162,29 +142,19 @@ function Header() {
               aria-label="User menu"
             >
               <span className="sr-only">Open user menu</span>
-              <Image
-                className="w-8 h-8 rounded-full"
-                src={img1}
-                width={50}
-                height={50}
-                alt="user photo"
-              />
+              <Image className="w-8 h-8 rounded-full" src={img1} width={50} height={50} alt="user photo" />
             </button>
 
             {/* منوی کاربری */}
             <div
               id="user-dropdown"
               className={`z-50 absolute top-10 right-0 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600 ${
-                showUserMenu ? "" : "hidden"
+                showUserMenu ? '' : 'hidden'
               }`}
             >
               <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
-                </span>
-                <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
-                  name@flowbite.com
-                </span>
+                <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                <span className="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
               </div>
               <ul className="py-2" aria-labelledby="user-menu-button">
                 <li>
@@ -224,20 +194,13 @@ function Header() {
           </div>
 
           {/* منوی اصلی */}
-          <div
-            className={`items-center justify-between ${
-              !isOpenMenu && "hidden"
-            } w-full md:flex md:w-auto md:order-1`}
-            id="navbar-user"
-          >
+          <div className={`items-center justify-between ${!isOpenMenu && 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-user">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
                   href="/"
                   className={`block py-2 px-3 ${
-                    pathname === "/"
-                      ? "text-white bg-blue-700 md:hover:text-blue-100"
-                      : "text-gray-900"
+                    pathname === '/' ? 'text-white bg-blue-700 md:hover:text-blue-100' : 'text-gray-900'
                   } rounded-sm md:p-1 md:dark:text-blue-500`}
                 >
                   صفحه اصلی
@@ -247,9 +210,7 @@ function Header() {
                 <Link
                   href="/shopping"
                   className={`block py-2 px-3 ${
-                    pathname === "/shopping"
-                      ? "text-white  bg-blue-700 md:hover:text-blue-100"
-                      : "text-gray-900"
+                    pathname === '/shopping' ? 'text-white  bg-blue-700 md:hover:text-blue-100' : 'text-gray-900'
                   } rounded-sm md:p-1 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
                 >
                   فروشگاه
@@ -259,9 +220,7 @@ function Header() {
                 <Link
                   href="/basket"
                   className={`block py-2 px-3 ${
-                    pathname === "/basket"
-                      ? "text-white bg-blue-700 md:hover:text-blue-100"
-                      : "text-gray-900"
+                    pathname === '/basket' ? 'text-white bg-blue-700 md:hover:text-blue-100' : 'text-gray-900'
                   } rounded-sm md:p-1 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
                 >
                   سبد خرید
@@ -280,13 +239,7 @@ function Header() {
             aria-expanded={isOpenMenu}
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path
                 fillRule="evenodd"
                 d="M3 5a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5zm0 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9zm0 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1z"
@@ -296,9 +249,9 @@ function Header() {
           </button>
         </div>
       </nav>
-      <div id="blur" className={`w-full h-screen fixed top-0 left-0 bg-gray-400/25 z-20 ${ !isOpenBasket ? "hidden" : ""}`}></div>
+      <div id="blur" className={`w-full h-screen fixed top-0 left-0 bg-gray-400/25 z-20 ${!isOpenBasket ? 'hidden' : ''}`}></div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
