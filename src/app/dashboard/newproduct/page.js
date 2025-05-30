084e28aed0a553c4b3120b3a16505902f5bebef7
mehrdad <126550362+mehrdadosouli@users.noreply.github.com>
2025-05-31 00:16:42 +0330

    Update page.js

--- src/app/dashboard/newproduct/page.js
+++ src/app/dashboard/newproduct/page.js
@@ -35,7 +35,7 @@ function Newproduct() {
     }
     if (formData.title && formData.price && formData.description) {
       try {
-        const res = await fetch('/api/products', {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
