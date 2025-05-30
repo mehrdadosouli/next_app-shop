0a2749165bcfc51f583517e6ce7611c6a2fad6f9
mehrdad <126550362+mehrdadosouli@users.noreply.github.com>
2025-05-31 00:37:13 +0330

    Update dbConnect.js

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -6,7 +6,7 @@ async function dbConnect() {
   }
 
   // return mongoose.connect('mongodb://localhost:27017/next-app', {
-  return mongoose.connect('mongodb+srv://mehrdadosoulisaran:Mehrdadosoulisaran@next-app.elehtpg.mongodb.net/?retryWrites=true&w=majority&appName=Next-app', {
+  return mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
