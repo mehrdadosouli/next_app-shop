532abc1e7ebd43853ad015b9796be48a8be64889
mehrdad <126550362+mehrdadosouli@users.noreply.github.com>
2025-05-31 00:18:31 +0330

    Update getProductDetails.jsx

--- src/components/getProductDetails/getProductDetails.jsx
+++ src/components/getProductDetails/getProductDetails.jsx
@@ -2,7 +2,7 @@
 
 async function getProductDetails(shoppingId) {
 
-  const response = await fetch(`/api/products/${shoppingId}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${shoppingId}`, {
     cache: "no-store",
   }).then((res) => res.json());
   return response
